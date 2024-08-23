692884a21350a11cb5b620ebbb9bde25688f1b9e
Zandar Beker (1000126240) <BEKEZA1@student.op.ac.nz>
2024-08-23 14:46:07 +1200

    cookie clicker updates

--- cookieclicker.js
+++ cookieclicker.js
@@ -1,10 +1,55 @@
-randomize.addEventListener('click', cookies);
+let cursors = 0;
 let cookies = 0;
 
-function cookies()
+
+
+function numberUpdater()
 {
 
-    cookies = cookies + 1;
-    cookienumber.textContent = cookies
+    const button = document.querySelector('.bigcookie');
+    const cookieNumberElement = document.querySelector('.cookienumber');
+    const button1 = document.querySelector('.buycursor');
+    const cursorsNumberElement = document.querySelector('.cursornumber');
 
+        cookieNumberElement.textContent = cookies;
+        cursorsNumberElement.textContent = cursors;
+
+        if (cursors >= 1)
+            {
+                setInterval(() => {
+                    cookies += 1;
+                    cookieNumberElement.textContent = cookies; 
+                }, 10000);
+            
+            }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.bigcookie');
+    const cookieNumberElement = document.querySelector('.cookienumber');
+
+    button.addEventListener('click', () => {
+        cookies++;
+        cookieNumberElement.textContent = cookies;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const button1 = document.querySelector('.buycursor');
+    const cursorsNumberElement = document.querySelector('.cursornumber');
+
+
+    button1.addEventListener('click', () => {
+        if (cookies <= 9)
+        {}
+        else
+        {
+            cursors++;
+            cookies -= 10
+            numberUpdater()
+        }
+      
+    });
+});
