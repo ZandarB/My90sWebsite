41d3c0be5bafbbc83e74534ac1be201a79311ee5
Zandar Beker (1000126240) <BEKEZA1@student.op.ac.nz>
2024-09-10 11:43:23 +1200

    done API stuff

--- apistuff.js
+++ apistuff.js
@@ -1,24 +1,55 @@
 const pokemonName = document.querySelector('.pokename');
+const br = document.createElement('br');
+let num = 0;
+
 
 async function getPokemon()
 {
     console.log("Working");
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=2');
     const data = await response.json();
     console.log(data.results);
     doStuff(data.results);
 }
 
+
+
 function doStuff(data){
 
     data.forEach(pokemon => {
         const div = document.createElement('div')
+        const button = document.createElement('button');
+        button.style.width = '100px';
+        button.style.height = '25px';
+
+
+        button.textContent = pokemon.name;
         div.innerHTML = pokemon.name;
+
         document.body.appendChild(div);
+        document.body.appendChild(button);
+
         console.log(pokemon.name);
-    });
-    
+        console.log("url", pokemon.url);
+
+        let url = pokemon.url;
+        button.addEventListener('click', () => {
+            specificPokemon(url);
+        })
+    }); 
 }
+
+
+async function specificPokemon(url)
+{
+
+    console.log 
+            const response2 = await fetch(url);
+            const specific = await response2.json(); 
+            console.log(specific.forms.name);
+}
+
+
 
 getPokemon();
 
